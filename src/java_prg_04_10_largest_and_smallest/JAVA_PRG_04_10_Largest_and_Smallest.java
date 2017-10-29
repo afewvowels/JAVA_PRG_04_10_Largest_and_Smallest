356959158eeb136084b2e770e47b0a4d5152f6e6
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-29 11:58:57 -0400

    Prompt written. Started.

---
+++ src/java_prg_04_10_largest_and_smallest/JAVA_PRG_04_10_Largest_and_Smallest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_04_10_largest_and_smallest;
+
+/**
+ *
+ * @author bluebackdev
+ * Write a program with a loop that lets the user enter a series of integers.
+ * The user should enter -99 to signal the end of the series. After all the
+ * numbers have been entered, the program should display the largest and
+ * smallest numbers entered.
+ */
+public class JAVA_PRG_04_10_Largest_and_Smallest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
